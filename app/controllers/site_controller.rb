b321b48c19378efc236a00dc59f7855f174bdadd
Gianna Fusaro <giannafusaro389@gmail.com>
2015-01-30 22:12:50 -0800

    make a site helper to determine whether otto's is open or closed

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
 
   def index
-    
+
   end
 
 end
--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,16 @@
 module SiteHelper
+  def is_open?
+    #M-Th
+    if Time.now.day > 1 and Time.now.day < 6
+      unless Time.now.day == 5
+        if Time.now.hour < 17 and Time.now.hour > 8
+          return true
+        end
+      end
+      if Time.now.hour < 15 and Time.now.hour > 8
+        return true
+      end
+    end
+    return false
+  end
 end
